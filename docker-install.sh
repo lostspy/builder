e3623cab40110d54583b465c62518caf4923ff27
Naresh <81670434+lostspy@users.noreply.github.com>
2022-04-25 09:17:34 +0530

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -9,30 +9,29 @@ sudo atomic-openshift-docker-excluder exclude
 if [ $? == 0 ]; then
     echo "Remove the docker cofig file"
     sudo rm -vf /root/.docker/config.json
+    echo ''
     echo "Configure Docker storage driver"
     sudo sed -i 's/STORAGE_DRIVER=.*$/STORAGE_DRIVER=devicemapper/' /etc/sysconfig/docker-storage-setup
     echo ""
     echo "Configure Docker storage"
     sudo sed -n -e '/^VG=/!p' -e '$aVG=docker' /etc/sysconfig/docker-storage-setup
+    echo ''
     echo "Configure storage data size"
     sudo sed -n -e '/^DATA_SIZE=/!p' -e '$aDATA_SIZE=80%FREE' /etc/sysconfig/docker-storage-setup
+    echo ''
     echo "Setup docker storage script"
     sudo docker-storage-setup
+    echo ''
     echo "restart the docker service"
     sudo systemctl restart docker
+    echo ''
     echo "restart the atomic service"
     sudo systemctl restart atomic-openshift-node
+    echo ''
 else
     echo "Error in Excluding the docker service"
     exit 1
 fi
-
-#echo "Marking nodes as schedulable"
-
-
-#echo "Restart the master api services on the node"
-#sudo /usr/local/bin/master-restart api
-#sudo /usr/local/bin/master-restart controllers
 echo ""
 
 echo "wait for 30 sec"
@@ -52,7 +51,6 @@ echo "Check openshift process and status"
 STATUS="$(sudo systemctl is-active atomic-openshift-node)"
 if [ "${STATUS}" == "active" ]; then
     echo "atomic-openshift-node is in Active state"
-    exit 1
 else
     echo "atomic-openshift-node is not running"
     exit 1
